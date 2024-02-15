cf32a6e226090dfb8dc4a89f4d82bb73159f1563
mariana549 <marievaristo889@gmail.com>
2024-02-15 15:20:49 -0300

    atualização

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,3 @@
 const botao = document.querySelector('#btn-search')
+const url = `https://api.github.com/users`
 
-async function pegarAPI () {
-   const url = `https://api.github.com`
-   const response =  await fetch(url)
-   console.log(response)
-   return response.json
-}
-pegarAPI()
-
-async function pegarUsuarios (users) {
-   const api = pegarAPI()
-   const url = `https://api.github.com/${users}`
-   const response = await fetch(url)
-   console.log(response)
-   return response.json
-}
-pegarUsuarios()
