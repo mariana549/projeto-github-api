1916552ad4bd4902102d2968e8adb32087483167
mariana549 <marievaristo889@gmail.com>
2024-02-15 15:14:09 -0300

    update.js

---
+++ src/scripts/index.js
@@ -0,0 +1,16 @@
+async function pegarAPI () {
+   const url = `https://api.github.com`
+   const response =  await fetch(url)
+   console.log(response)
+   return response.json
+}
+pegarAPI()
+
+async function pegarUsuarios (users) {
+   const api = pegarAPI()
+   const url = `https://api.github.com/${users}`
+   const response = await fetch(url)
+   console.log(response)
+   return response.json
+}
+pegarUsuarios()
