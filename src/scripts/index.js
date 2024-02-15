c3be25d74b36542d1116265070dfe37b764f0460
mariana549 <marievaristo889@gmail.com>
2024-02-15 15:42:50 -0300

    feat: :zap: test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,20 @@
 const botao = document.querySelector('#btn-search')
 const url = `https://api.github.com/users`
 
-async function pegarUsuarios () {
-   const resposta = await fetch(url)
-   const data = await resposta.json
+async function pegarUsuarios (users) {
+   const response = await fetch(url)
+   const data = await response.json
+   console.log(response)
 
-   const inputText = document.querySelector('#input-search')
+   users = response.headers
+   console.log(users)
+
+   // const inputText = document.querySelector('#input-search')
+
+   // async function digitarIdUsuario (){
+   //    const usuario = inputText.value
+
+   // }
+   // digitarIdUsuarios()
 }
+pegarUsuarios()
