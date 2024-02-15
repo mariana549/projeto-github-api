5812b6c03cc96b4bf694d0a94f39c94461ce1872
mariana549 <marievaristo889@gmail.com>
2024-02-15 15:27:12 -0300

    atualizações

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 const botao = document.querySelector('#btn-search')
 const url = `https://api.github.com/users`
 
+async function pegarUsuarios () {
+   const resposta = await fetch(url)
+   const data = await resposta.json
+
+   const inputText = document.querySelector('#input-search')
+}
