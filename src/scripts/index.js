4a72c54404ae2a0a41454dbe5f9f6fc117dfa4f7
mariana549 <marievaristo889@gmail.com>
2024-02-17 10:39:28 -0300

    atualização

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,10 +6,25 @@ async function pegarUsuarios () {
    const response = await fetch(url)
    const data = await response.json()
    console.log(data)
-   const {avatar_URL, bio, name, repos_url, 
-      public_repos} = data 
-   console.log(bio)
+   const {avatar_url, bio, name, repos_url} = data 
+   
    const perfil = document.querySelector('.profile-data')
+   perfil.innerHTML = `
+   <div class="info">
+   <img src="${avatar_url}" alt="foto de perfil"/>
+   <div class="data">
+      <h1>${name}</h1>
+      <p>${bio}</p>
+   </div>
+   </div>
+   <div class="repositories">
+      <h2>Repositórios</h2>
+      <ul>
+         <li><a href="${repos_url[i]}" target="_black">${repos_url[i].name }</li>
+      </ul>
+</div>`
+
 
 }
 console.log(await pegarUsuarios())
+
